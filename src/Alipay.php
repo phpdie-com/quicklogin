6654f2856dbcfc626c433f2c9d828dc65ac1a063
phpdie-com <458638670@qq.com>
2021-06-13 18:24:51 +0800

    Update Alipay.php

--- src/Alipay.php
+++ src/Alipay.php
@@ -23,11 +23,8 @@ class Alipay extends AbstractOauth
 
     public function getAccessToken($code, $refresh_token = '')
     {
-        $param = $this->buildRequestParam('alipay.system.oauth.token', false, false, $code);
+        $param = $this->buildRequestParam('alipay.system.oauth.token', $code);
         $result = Curl::post('https://openapi.alipay.com/gateway.do', $param);
-
-        var_dump('请求getAccessToken接口结果', $result);
-
         $result = json_decode($result, true);
         if (!empty($result['alipay_system_oauth_token_response']['access_token'])) {
             return $result['alipay_system_oauth_token_response']['access_token'];
@@ -36,7 +33,7 @@ class Alipay extends AbstractOauth
 
     public function  getUserInfo($auth_token)
     {
-        $param = $this->buildRequestParam('alipay.user.info.share', false, false, $auth_token);
+        $param = $this->buildRequestParam('alipay.user.info.share', $auth_token);
         $result = Curl::post('https://openapi.alipay.com/gateway.do', $param);
         return $result ? json_decode($result, true) : [];
     }
@@ -56,30 +53,22 @@ class Alipay extends AbstractOauth
         return base64_encode($sign);
     }
 
-    //$code, $refresh_token, $auth_token三者只需要传一种,其他2种留false即可
-    private function buildRequestParam($method, $code, $refresh_token, $auth_token)
+    private function buildRequestParam($method, $code, $refresh_token = '')
     {
-        if ($code) {
-            $param['code'] = $code;
-            $param['grant_type'] = 'authorization_code';
-        } else if ($refresh_token) {
+        if ($refresh_token) {
             $param['refresh_token'] = $refresh_token;
             $param['grant_type'] = 'refresh_token';
-        } else if ($auth_token) {
-            $param['code'] = $auth_token;
+        } else {
+            $param['code'] = $code;
             $param['grant_type'] = 'authorization_code';
         }
+        $param['app_id'] = $this->appID;
         $param['method'] = $method;
         $param['charset'] = 'utf-8';
+        $param['sign_type'] = 'RSA2';
         $param['timestamp'] = date('Y-m-d H:i:s');
         $param['version'] = '1.0';
-        $param['sign_type'] = 'RSA2';
-        $param['app_id'] = $this->appID;
-        $param_copy=$param;
-        $signStr = $this->signData($param_copy);
-        $param['sign'] = $signStr;
-
-        var_dump('签名字符串'.$signStr);
+        $param['sign'] = $this->signData($param);
         return $param;
     }
 }
